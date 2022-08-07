7b017d69990e1dd07d0288e9ee427b24ac90d563
leiasalcido <leilannyas@gmail.com>
2022-08-06 23:36:36 -0700

    Added search engine

--- src/app.js
+++ src/app.js
@@ -45,8 +45,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8f445a7c7ea5a3d052241482838ca33d";
-let city = "Phoenix";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "8f445a7c7ea5a3d052241482838ca33d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Phoenix");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
