4bea54bfeca00c022fa080f1890aebfdce4229c1
leiasalcido <leilannyas@gmail.com>
2022-08-12 11:04:41 -0700

    Updated apiUrl to include correct API version.

--- src/app.js
+++ src/app.js
@@ -72,7 +72,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "8f445a7c7ea5a3d052241482838ca33d";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
